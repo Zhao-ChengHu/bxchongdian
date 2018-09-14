3cca2b66a0df94d4f2edff520a5fcaadc6fcc905
zhaochenghu <kevin_cheng_hu@163.com>
2018-09-14 13:54:19 +0800

    百祥项目提交

---
+++ app/src/main/java/com/bxchongdian/app/wxapi/WXEntryActivity.java
@@ -0,0 +1,10 @@
+package com.bxchongdian.app.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+/*
+* zhaochenghu
+* */
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+}
